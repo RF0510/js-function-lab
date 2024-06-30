2cb3b9f877c352c0f4bfe0ffed11d7c89ad1aa72
RF0510 <rosevelt.foushee@yahoo.com>
2024-06-30 15:29:36 -0400

    js-function-lab-finish-exercise2

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-//const maxOfThree = function (x, y, z){
-//if (x >= y && x >= z)
-//{ return x; }
-//else if (y >= x && y >= z)
-// { return y; }
-//else 
-//{ return z; }
-//}
-//console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+/*
+Exercise 1: maxOfTwoNumbers()
 
-function percentage (x) {
-{ return x * 100; }
-} 
-const tipPercentage = percentage(2);
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
 
-
-const calculateTip = (x, y) => {
-{ return x * y; }
-}
-console.log('Exercise 7 Result:', calculateTip(20, 2));
-
-/*Exercise 7: calculateTip()
-
-Create a function called calculateTip. It should take two arguments: git 
-the bill amount and the tip percentage (as a whole number). 
-The function should return the amount of the tip.
-
-Example: calculateTip(50, 20) should return 10.
+Exercise 1 has been completed for you:
 */
+
+//const maxOfTwoNumbers = (x, y) => {
+//  if (x >= y) {
+//  return x;
+//} else {
+//return y;
+//}
+//}
+
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+function isAdult(number) {
+    if (number >= 18)
+        return "Adult";
+    else return "Minor";
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
